7067f4c8708399aecd6196e21ba02f1cb0d760c4
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-06-10 22:23:41 +0500

    yandex credentials

--- config/initializers/02_yandex.rb
+++ config/initializers/02_yandex.rb
@@ -1,5 +1,5 @@
 YandexPhotoStorage.configure do |config|
-  # config.api_key = ''
-  # config.api_secret = ''
+  config.api_key = Rails.application.credentials.yandex.fetch(:api_key)
+  config.api_secret = Rails.application.credentials.yandex.fetch(:api_secret)
   config.logger = Logger.new(Rails.root.join('log', "yandex_#{Rails.env}.log"))
 end
