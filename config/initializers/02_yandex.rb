e0d7c3f9f24d467299f61b669425bf27160b070b
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-06-11 10:33:20 +0500

    favicon and admin styles

--- config/initializers/02_yandex.rb
+++ config/initializers/02_yandex.rb
@@ -1,5 +1,8 @@
 YandexPhotoStorage.configure do |config|
-  config.api_key = Rails.application.credentials.yandex.fetch(:api_key)
-  config.api_secret = Rails.application.credentials.yandex.fetch(:api_secret)
+  unless Rails.env.test?
+    config.api_key = Rails.application.credentials.yandex.fetch(:api_key)
+    config.api_secret = Rails.application.credentials.yandex.fetch(:api_secret)
+  end
+
   config.logger = Logger.new(Rails.root.join('log', "yandex_#{Rails.env}.log"))
 end
