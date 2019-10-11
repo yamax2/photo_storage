9b8998c6bb0c9cd0f46e859b1067aca3a1507162
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-10-11 11:36:02 +0500

    move to ruby 2.6.5 in prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,5 +33,5 @@ set :ssh_options, verify_host_key: :secure
 set :ssh_options, forward_agent: true, user: 'photos'
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.3'
+set :rbenv_ruby, '2.6.5'
 set :default_env, {disable_binstubs: '1'}
