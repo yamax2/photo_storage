99461569c6f06d1c1166db677c92614ea22cff41
Maxim Tretyakov <max@tretyakov-ma.ru>
2020-02-25 02:37:18 +0500

    yandex token finder

---
+++ app/services/yandex/token_for_upload_service.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Yandex
+  class TokenForUploadService
+    include ::Interactor
+
+    CACHE_REDIS_KEY = 'yandex_tokens_usage'
+    FIND_SCRIPT = <<~LUA
+      local cache_values = redis.call("HGETALL", KEYS[1])
+      local cache = {}
+      for idx = 1, #cache_values, 2 do
+        cache[cache_values[idx]] = cache_values[idx + 1]
+      end
+      local info = cjson.decode(ARGV[1])
+      local result = {}
+      for id, space in pairs(info) do
+        local current_space = cache[id] or 0
+        current_space = current_space + ARGV[2]
+        if current_space < info[id] then
+          table.insert(result, id)
+        end
+      end
+      if table.getn(result) > 0 then
+        local id = math.min(unpack(result))
+        redis.call("HINCRBY", KEYS[1], id, ARGV[2])
+        return id
+      end
+    LUA
+
+    private_constant :FIND_SCRIPT
+    delegate :resource_size, :token_id, to: :context
+
+    def call
+      return if actual_tokens.empty?
+
+      context.token_id = RedisScriptService.
+        new(FIND_SCRIPT).
+        call(
+          keys: CACHE_REDIS_KEY,
+          argv: [actual_tokens.to_json, resource_size]
+        ).to_i
+    end
+
+    private
+
+    def actual_tokens
+      return @actual_tokens if defined?(@actual_tokens)
+
+      table = Token.arel_table
+      free_space = Arel::Nodes::Subtraction.new(table[:total_space], table[:used_space])
+
+      @actual_tokens = Token.active.where(free_space.gt(resource_size)).pluck(:id, free_space).to_h
+    end
+  end
+end
---
+++ spec/services/yandex/token_for_upload_service_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Yandex::TokenForUploadService do
+  let(:redis) { RedisClassy.redis }
+
+  let(:service_context) { described_class.call!(resource_size: 100.megabytes) }
+  let(:token_id) { service_context.token_id }
+
+  context 'when without tokens' do
+    it { expect(token_id).to be_nil }
+  end
+
+  context 'when without active tokens' do
+    let!(:token) { create :'yandex/token', total_space: 150.megabytes, used_space: 10.megabytes, active: false }
+
+    it { expect(token_id).to be_nil }
+  end
+
+  context 'when without actual tokens' do
+    let!(:token) { create :'yandex/token', total_space: 150.megabytes, used_space: 130.megabytes, active: true }
+
+    it { expect(token_id).to be_nil }
+  end
+
+  context 'when some tokens exist' do
+    let!(:token1) { create :'yandex/token', total_space: 150.megabytes, used_space: 0, active: false }
+    let!(:token2) { create :'yandex/token', total_space: 150.megabytes, used_space: 30.megabytes, active: true }
+    let!(:token3) { create :'yandex/token', total_space: 150.megabytes, used_space: 30.megabytes, active: true }
+
+    context 'and first call' do
+      it { expect(token_id).to eq(token2.id) }
+    end
+
+    context 'and second call' do
+      before { described_class.call!(resource_size: 100.megabytes) }
+
+      it { expect(token_id).to eq(token3.id) }
+    end
+  end
+
+  context 'when no free space on first token' do
+    let!(:token1) { create :'yandex/token', total_space: 150.megabytes, used_space: 130.megabytes, active: true }
+    let!(:token2) { create :'yandex/token', total_space: 150.megabytes, used_space: 30.megabytes, active: true }
+
+    it { expect(token_id).to eq(token2.id) }
+  end
+
+  context 'when no free space' do
+    let!(:token1) { create :'yandex/token', total_space: 150.megabytes, used_space: 130.megabytes, active: true }
+    let!(:token2) { create :'yandex/token', total_space: 150.megabytes, used_space: 140.megabytes, active: true }
+
+    it { expect(token_id).to be_nil }
+  end
+end
