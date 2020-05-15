f9af54e992293e0529c451a28a668f311d62bec9
Maxim Tretyakov <max@tretyakov-ma.ru>
2020-05-16 03:24:16 +0500

    remove proxy_session rspec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,6 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include StripAttributes::Matchers
-  config.include ProxySessionHelper
 
   config.use_transactional_fixtures = true
 
