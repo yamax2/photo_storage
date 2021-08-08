1aced03eb89241e7ed31550784f8124e4fdad3f5
Maxim Tretyakov <max@tretyakov-ma.ru>
2021-08-09 01:58:54 +0500

    LoadNode method for proxy

--- proxy_service/internal/nodes.go
+++ proxy_service/internal/nodes.go
@@ -2,29 +2,106 @@ package internal
 
 import (
 	"time"
+	"sync"
+	"fmt"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
 )
 
 const nodeTTL = time.Hour
 
 type Node struct {
-	Type 		string
-	Name		string
-	Secret		string
+	Type 		string		`json:"type"`
+	Name		string		`json:"name"`
+	Secret		string		`json:"secret"`
 	LoadedAt 	time.Time
 }
 
-var nodes map[int64]Node
+func (node *Node) IsExpired() bool {
+	return node.LoadedAt.Add(nodeTTL).Before(time.Now())
+}
+
+func (node *Node) Valid() bool {
+	return len(node.Type) > 0 && len(node.Name) > 0 && len(node.Secret) > 0
+}
+
+var (
+	nodes map[int64]Node
+	mx sync.RWMutex
+	httpClient = http.Client{
+		Timeout: time.Duration(1 * time.Second),
+	}
+)
 
 func init() {
 	nodes = make(map[int64]Node)
 }
 
 func GetNode(id int64) *Node {
-	value, ok := nodes[id]
+	mx.RLock()
+	defer mx.RUnlock()
 
-	if ok && (value.LoadedAt.Add(nodeTTL).After(time.Now())) {
-		return &value
+	node, ok := nodes[id]
+
+	if ok && !node.IsExpired() {
+		return &node
 	} else {
 		return nil
 	}
+}
+
+func LoadNode(c context.Context, id int64) (*Node, error) {
+	mx.Lock()
+	defer mx.Unlock()
+
+	node, ok := nodes[id]
+
+	if ok && !node.IsExpired()  {
+		return &node, nil
+	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/admin/nodes/%d", config.ApiHost, id), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req = req.WithContext(c)
+
+	var resp *http.Response
+	resp, err = httpClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Api response is %s", http.StatusText(resp.StatusCode))
+	}
+
+	var content []byte
+	defer resp.Body.Close()
+
+	content, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	node = Node{}
+	err = json.Unmarshal(content, &node)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !node.Valid() {
+		return nil, fmt.Errorf("Node is not valid: %d", id)
+	}
+
+	node.LoadedAt = time.Now()
+	nodes[id] = node
+
+	return &node, nil
 }
--- proxy_service/internal/nodes_test.go
+++ proxy_service/internal/nodes_test.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"testing"
+	"context"
 	"time"
+	"net/http"
+	"net/http/httptest"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -37,4 +40,175 @@ func TestGetNode(t *testing.T) {
 	t.Cleanup(func() {
 		nodes = make(map[int64]Node)
 	})
+}
+
+func TestLoadNode(t *testing.T) {
+	var nodeApiStub *httptest.Server
+
+	LoadConfig()
+
+	t.Run("when first time load", func(t *testing.T) {
+		nodeApiStub = defaultStub()
+		config.ApiHost = nodeApiStub.URL
+
+		node, err := LoadNode(context.Background(), 1)
+
+		assert.Nil(t, err)
+
+		assert.Equal(t, node.Name, "test")
+		assert.Equal(t, node.Type, "yandex")
+		assert.Equal(t, node.Secret, "secret")
+		assert.Equal(t, node.IsExpired(), false)
+	})
+
+	t.Run("when expired node exists", func(t *testing.T) {
+		nodeApiStub = defaultStub()
+		config.ApiHost = nodeApiStub.URL
+
+		nodes[1] = Node{
+			Name: "zozo",
+			Type: "zozo",
+			Secret: "zozo",
+			LoadedAt: time.Now().Add(-5 * time.Hour),
+		}
+
+		assert.Nil(t, GetNode(1))
+
+		node, err := LoadNode(context.Background(), 1)
+
+		assert.Nil(t, err)
+		assert.Equal(t, node.Name, "test")
+		assert.Equal(t, node.IsExpired(), false)
+
+		assert.NotNil(t, GetNode(1))
+	})
+
+	t.Run("when timeout", func(t *testing.T) {
+		nodes = make(map[int64]Node)
+		httpClient = http.Client{
+			Timeout: time.Duration(5 * time.Millisecond),
+		}
+
+		nodeApiStub = httptest.NewServer(
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					time.Sleep(5 * time.Second)
+				},
+			),
+		)
+
+		config.ApiHost = nodeApiStub.URL
+		node, err := LoadNode(context.Background(), 1)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, node)
+
+		t.Cleanup(func() {
+			httpClient = http.Client{
+				Timeout: time.Duration(1 * time.Second),
+			}
+		})
+	})
+
+	t.Run("when invalid response", func(t *testing.T) {
+		nodes = make(map[int64]Node)
+		nodeApiStub = httptest.NewServer(
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					w.Write([]byte(`{"id": 1, "name": "test"}`))
+				},
+			),
+		)
+
+		config.ApiHost = nodeApiStub.URL
+		node, err := LoadNode(context.Background(), 1)
+
+		assert.Equal(t, err.Error(), "Node is not valid: 1")
+		assert.Nil(t, node)
+	})
+
+	t.Run("when api response is 500", func(t *testing.T) {
+		nodes = make(map[int64]Node)
+		nodeApiStub = httptest.NewServer(
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					w.WriteHeader(http.StatusInternalServerError)
+				},
+			),
+		)
+
+		config.ApiHost = nodeApiStub.URL
+		node, err := LoadNode(context.Background(), 1)
+
+		assert.Equal(t, err.Error(), "Api response is Internal Server Error")
+		assert.Nil(t, node)
+	})
+
+	t.Cleanup(func() {
+		nodes = make(map[int64]Node)
+		config = nil
+	})
+}
+
+func TestNode_Valid(t *testing.T) {
+	tests := []struct {
+		name   string
+		node   Node
+		want   bool
+	}{
+		{
+			"Valid with name, type and secret",
+			Node{
+				Type: "test",
+				Name: "test",
+				Secret: "test",
+			},
+			true,
+		},
+		{
+			"Invalid with all empty attrs",
+			Node{},
+			false,
+		},
+		{
+			"Invalid without name",
+			Node{
+				Type: "test",
+				Secret: "secret",
+			},
+			false,
+		},
+		{
+			"Invalid without secret",
+			Node{
+				Type: "test",
+				Name: "name",
+			},
+			false,
+		},
+		{
+			"Invalid without type",
+			Node{
+				Secret: "test",
+				Name: "name",
+			},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.node.Valid(), tt.want)
+		})
+	}
+}
+
+func defaultStub() *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte(`{"id": 1, "name": "test", "type": "yandex", "secret": "secret"}`))
+			},
+		),
+	)
 }
