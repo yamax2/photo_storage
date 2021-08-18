e8bd5d1b0a8ee2b197a29d454748fd8da53cbeca
Maxim Tretyakov <max@tretyakov-ma.ru>
2021-08-18 18:17:06 +0500

    Click on marker action

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Leaflet.tile_layer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-Leaflet.max_zoom = 18
+Leaflet.max_zoom = 19
 Leaflet.attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
